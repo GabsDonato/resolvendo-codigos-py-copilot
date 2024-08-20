1bcdd1e4ecf220c25177d1a0a873ad2e6da4da23
Gabriela Donato <146032843+GabsDonato@users.noreply.github.com>
2024-08-20 20:00:41 -0300

    Update ope_mat.py
    exercicio 3

--- resolucoes_code/ope_mat.py
+++ resolucoes_code/ope_mat.py
@@ -1,1 +1,19 @@
 # Vamos solicitar como entrada dois números e depois vamos realizar uma operação simples entre eles.
+
+nu1 = inte(input("digite o primeiro número:"))
+
+nu1 = inte(input("digite o segundo número:"))
+
+operacao = input("digite a operação que deseja realizar (+ , - ,* , /) : ")
+
+if operação == '+':
+print (num1 + num2)
+elif operacao == '+':
+elif operacao == '-':
+print(num1 - num2)
+elif operacao == '*':
+print(num1*num2)
+elif operacao == '/':
+
+print(num 1 / num2)
+print("operação invalida")
