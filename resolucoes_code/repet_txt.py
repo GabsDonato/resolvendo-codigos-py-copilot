1468d2937d07cb0d039d1c5f71315bca1c9ba3db
Gabriela Donato <146032843+GabsDonato@users.noreply.github.com>
2024-08-20 19:49:25 -0300

    Update repet_txt.py
    exercicio 2

--- resolucoes_code/repet_txt.py
+++ resolucoes_code/repet_txt.py
@@ -1,1 +1,5 @@
 # Vamos solicitar como entrada dois números e depois vamos realizar uma operação simples entre eles.
+string = input("digite uma string:")
+numero = inte(input("digite um número inteiro:")
+
+print(string * numero)
